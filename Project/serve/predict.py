b9f1ddf2fc70b4d65bb6a21fd409ff5d7c9f96c6
EC2 Default User <ec2-user@ip-172-16-61-237.us-east-2.compute.internal>
2020-05-10 17:54:54 +0000

    Upload from SageMaker-working predict on test review

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -88,5 +88,9 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
     
-    result = np.array(round(model(data)))
+    output = model(data)
+    
+    result = torch.round(output.squeeze())
     return result
+
+
